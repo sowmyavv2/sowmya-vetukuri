999af91d930f8dae7c941cd844ac2762d1ae4416
sowmyavv2 <your.email@example.com>
2024-10-13 14:26:20 -0400

    add header

---
+++ src/app/header/Header.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+const Header = () => {
+return (<>
+<div className="breadcrumb">Sowmya Vetukuri
+</div>
+    <header className='title'>
+    <img src='https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/15ce3a41-69ad-459e-92cd-35e27c2dede5/queenrender2/w=256,quality=90,fit=scale-down'></img>
+            </header>
+          
+
+</>)
+}
+
+export default Header;
